8f1e1b8a60eae31af73d53e8842a9a31751df6cb
Jacob Barta <jacob.barta25@gmail.com>
2023-02-22 15:19:13 -0500

    link detection working

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,21 +5,25 @@ import useEntry from '../networktables/useEntry';
 
 function GridEntry(props) {
 
+    let value = props.entry;
+    if(props.rowIndex == props.nextRow && props.colIndex == props.nextCol) value = "next";
+    if(props.link) value = "link"
+
     return (
-        <div
-            className={`entry ${props.entry}`}
+        <td
+            className={`entry ${value}`}
             onClick={() => props.handleClick(props.rowIndex, props.colIndex)}
-        ></div>
+        ></td>
     )
 }
 const Grid = () => {
     const [nextRow, setNextRow] = useEntry('/cone-ui/next/row', 0);
     const [nextCol, setNextCol] = useEntry('/cone-ui/next/col', 0);
 
-    let startGrid = Array(9);
+    let startGrid = Array(3);
 
     for(let i = 0; i<startGrid.length; i++) {
-        startGrid[i] = Array(3).fill("");
+        startGrid[i] = Array(9).fill("");
     }
 
     const [grid, setGrid] = useState(startGrid);
@@ -32,60 +36,101 @@ const Grid = () => {
             setNextCol(-1);
         }
 
-        if (newGrid[colIndex][rowIndex] === "") {
-            if (colIndex === 1 || colIndex === 4 || colIndex === 7) {
-                newGrid[colIndex][rowIndex] = "cube";
-            } else {
-                newGrid[colIndex][rowIndex] = "cone";
+        if (newGrid[rowIndex][colIndex] === "") {
+            if(rowIndex == 2) {
+                newGrid[rowIndex][colIndex] = "both";
+            }else {
+                if (colIndex === 1 || colIndex === 4 || colIndex === 7) {
+                    newGrid[rowIndex][colIndex] = "cube";
+                } else {
+                    newGrid[rowIndex][colIndex] = "cone";
+                }
             }
         } else {
-            newGrid[colIndex][rowIndex] = "";
+            newGrid[rowIndex][colIndex] = "";
         }
         setGrid(newGrid);
     };
 
     let nextType = determineNextType(nextCol)
 
+    let linkCoords = getLinkCoords(getLinks(grid));
+
+
     return (
         <div>
             <p className={nextType + "-next"}>Next: {nextType}</p>
-            <div className="grid">
-                {grid.map((row, colIndex) => (
-                    <div className="row" key={colIndex}>
-                        {row.map((entry, rowIndex) => (
-                            <GridEntry
-                                entry={rowIndex == nextRow && colIndex == nextCol ? "next" : entry} //TODO: encapsulate this logic in the entry more
-                                rowIndex={rowIndex}
-                                colIndex={colIndex}
-                                handleClick={handleClick}
-                                key={rowIndex.toString() + colIndex.toString()}
-                            />
-                        ))}
-                    </div>
-                ))}
-            </div>
+            <table className="grid">
+                    {grid.map((row, rowIndex) => (
+                        <tr className="grid-row" key={rowIndex}>
+                            {row.map((entry, colIndex) => (
+                                    <GridEntry
+                                        entry={entry}
+                                        link={isPartOfLink(rowIndex, colIndex, linkCoords)}
+                                        nextRow={nextRow}
+                                        nextCol={nextCol}
+                                        rowIndex={rowIndex}
+                                        colIndex={colIndex}
+                                        handleClick={handleClick}
+                                        key={rowIndex.toString() + colIndex.toString()}
+                                    />
+                            ))}
+                        </tr>
+                    ))}
+           </table>
         </div>
     );
 };
 
 function determineNextType(col) {
     return col == 1 || col == 4 || col == 7 ? "cube" : "cone"
+
+    //TODO: make this respect ambiguity on bottom row
 }
 
-function transpose(columnByRow) {
-    let newArr = new Array(columnByRow[0].length);
+function getLinks(grid) {
 
-    for(let i = 0; i<newArr.length; i++) {
-        newArr[i] = new Array(columnByRow.length)
-    }
+    let links = new Array();
 
-    for(let i = 0; i<columnByRow.length; i++) {
-        for( let j = 0; j<columnByRow[0].length; j++) {
-            newArr[j][i] = columnByRow[i][j]
+    let numLinks = 0;
+
+    for(let row = 0; row<grid.length; row++) {
+        for(let col = 0; col<grid[0].length-2; col++) {
+            if(gridSpaceFilled(grid[row][col]) &&
+                gridSpaceFilled(grid[row][col+1]) &&
+                gridSpaceFilled(grid[row][col+2])) {
+                numLinks++;
+
+                links.push({
+                    id: numLinks,
+                    row: row,
+                    col1: col,
+                    col2: col+1,
+                    col3: col+2
+                })
+
+                col+= 2
+            }
         }
     }
 
-    return newArr;
+    return links;
+}
+
+function getLinkCoords(links) {
+    let coords = new Array();
+    for(let i = 0; i<links.length; i++) {
+        let thisLink = links[i];
+        coords.push({row: thisLink.row, col: thisLink.col1});
+        coords.push({row: thisLink.row, col: thisLink.col2});
+        coords.push({row: thisLink.row, col: thisLink.col3});
+    }
+
+    return coords;
+}
+
+function isPartOfLink(row, col, linkCoords) {
+    return linkCoords.filter((e) => e.row == row && e.col == col).length > 0
 }
 
 function evaluateNext(grid, numLinks, linkBonusAttained) {
@@ -96,14 +141,6 @@ function evaluateNext(grid, numLinks, linkBonusAttained) {
 
     } else {
         return nextHighest(gridTranspose)
-    }
-}
-
-function getLinks(grid) {
-    let transpose = transpose(grid);
-
-    for(let row = 0; row<transpose; row++) {
-        
     }
 }
 
